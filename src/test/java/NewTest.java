af7021ae6f194aa51620fe161b425028019ea8e2
Smedom <139339332+Smedom@users.noreply.github.com>
2023-08-21 09:32:04 +0400

    Update NewTest.java

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 public class NewTest {
     @Test
     void newTest(){
-        int i = 3;
+        int i = 42;
         System.out.println("This is the best test");
         Assertions.assertTrue(i > 2);
     }
